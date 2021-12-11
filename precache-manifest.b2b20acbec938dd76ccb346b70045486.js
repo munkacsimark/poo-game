a1bfec21fc1f3574a4aaa63c4b4e0d1046498eb9
munkacsimark <munkacsimark@users.noreply.github.com>
2021-12-11 18:00:31 +0000

    deploy: 61ebf37409b77ac1b97e9c0609b2f1e70415ab8e

---
+++ precache-manifest.b2b20acbec938dd76ccb346b70045486.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1ba8f434b0416747d3c0e3758aa972",
+    "url": "/poo-game/index.html"
+  },
+  {
+    "revision": "86aecd40dd5d2aedd1ef",
+    "url": "/poo-game/static/css/main.689bd775.chunk.css"
+  },
+  {
+    "revision": "5d67f6e32eea5cae46fd",
+    "url": "/poo-game/static/js/2.7af17b06.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/poo-game/static/js/2.7af17b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86aecd40dd5d2aedd1ef",
+    "url": "/poo-game/static/js/main.ab844ebf.chunk.js"
+  },
+  {
+    "revision": "67b4be0888d0daab463e",
+    "url": "/poo-game/static/js/runtime-main.e9eb01b3.js"
+  },
+  {
+    "revision": "c1b7fb754ec78a23f63c22f1322ac273",
+    "url": "/poo-game/static/media/09037.c1b7fb75.mp3"
+  },
+  {
+    "revision": "f74cd93b7a5d6ecd5476fe6d6aa96ea3",
+    "url": "/poo-game/static/media/courtney-kenady.f74cd93b.jpg"
+  },
+  {
+    "revision": "d28434f1794ba1c0fffc983076b3952f",
+    "url": "/poo-game/static/media/dry-fart.d28434f1.mp3"
+  },
+  {
+    "revision": "84eaee600f040ed7e304c304be6a365d",
+    "url": "/poo-game/static/media/fart-sound.84eaee60.mp3"
+  },
+  {
+    "revision": "c6becc38425e90e90d10c3236f1b5bce",
+    "url": "/poo-game/static/media/perfect-fart.c6becc38.mp3"
+  }
+]);
